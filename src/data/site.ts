a7c4fb66b134df9895c1d560035645830b61df82
CalienteSneakers <viickthoor96+Calientesneakers@gmail.com>
2026-03-17 17:17:13 +0100

    update

--- src/data/site.ts
+++ src/data/site.ts
@@ -9,19 +9,19 @@ export const home = {
   title: "Caliente\nSneakers",
   subtitle: "Bienvenido a",
   description:
-    "Este espacio no es para cualquiera. Aquí no hablamos solo de calzado...",
+    "Este espacio no es para cualquiera. Aquí no hablamos solo de calzado, hablamos de pasión, cultura y estilo. Si las zapatillas son parte de tu vida, si te emocionas con cada nuevo lanzamiento, si cuidas tus pares como un tesoro y te interesa todo lo que rodea a la moda urbana… entonces has llegado al lugar correcto.",
   features: [
     {
       title: "Guías exclusivas",
-      description: "👟 Aprende a cuidar tus zapatillas...",
+      description: "👟 Aprende a cuidar tus zapatillas, cómo combinarlas con tu estilo y a detectar las mejores ediciones.",
     },
     {
       title: "Accesorios recomendados",
-      description: "✨ Desde productos de limpieza...",
+      description: "✨ Desde productos de limpieza hasta complementos que potencian tu outfit.",
     },
     {
       title: "Mucho máss",
-      description: "🚀 Consejos y contenido creado...",
+      description: "🚀 Consejos y contenido creado para quienes de verdad sienten esta cultura.",
     },
   ],
 };
